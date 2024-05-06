05f5bffd8fbe89be3ef39877e6867221f577b73d
Taco <87836366+Tacotakedown@users.noreply.github.com>
2024-05-05 18:20:32 -0600

    fix cmake wanting to dynamic link minhook sometimes

--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,94 @@
 #include "main.h"
-extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND h_wnd, UINT msg, WPARAM w_param, LPARAM l_param);
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-Present o_present;
+Present oPresent;
 HWND window = NULL;
-WNDPROC o_wnd_proc;
-ID3D11Device* device_ptr = NULL;
-ID3D11DeviceContext* context_ptr = NULL;
-ID3D11RenderTargetView* main_render_target_view_ptr;
+WNDPROC oWndProc;
+ID3D11Device* pDevice = NULL;
+ID3D11DeviceContext* pContext = NULL;
+ID3D11RenderTargetView* mainRenderTargetView;
 
-void init_imgui() {
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    io.ConfigFlags = ImGuiConfigFlags_NoMouseCursorChange;
-    ImGui_ImplWin32_Init(window);
-    ImGui_ImplDX11_Init(device_ptr, context_ptr);
+void InitImGui()
+{
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags = ImGuiConfigFlags_NoMouseCursorChange;
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX11_Init(pDevice, pContext);
 }
 
-LRESULT __stdcall window_process(const HWND h_wnd, UINT u_msg, WPARAM w_param, LPARAM l_param) {
-    if(true && ImGui_ImplWin32_WndProcHandler(h_wnd, u_msg, w_param, l_param))
-    return true;
+LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
-    return CallWindowProc(o_wnd_proc, h_wnd, u_msg, w_param, l_param);
+	if (true && ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
+		return true;
+
+	return CallWindowProc(oWndProc, hWnd, uMsg, wParam, lParam);
 }
 
 bool init = false;
-HRESULT __stdcall present_hook(IDXGISwapChain* swap_chain_ptr, UINT sync_interval, UINT flags) {
-    if(!init){
-        if(SUCCEEDED(swap_chain_ptr->GetDevice(__uuidof(ID3D11Device),(void**)& device_ptr))){
-            device_ptr->GetImmediateContext(&context_ptr);
-            DXGI_SWAP_CHAIN_DESC sd; 
-            swap_chain_ptr -> GetDesc(&sd);
-            window = sd.OutputWindow;
-            ID3D11Texture2D* texture_buf_ptr;
-            swap_chain_ptr->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)& texture_buf_ptr);
-            device_ptr->CreateRenderTargetView(texture_buf_ptr, NULL, &main_render_target_view_ptr);
-            texture_buf_ptr->Release();
-            o_wnd_proc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)window_process);
-            init_imgui();
-            init = true;
-        }
-        else return o_present(swap_chain_ptr, sync_interval, flags);
-    }
+HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags)
+{
+	if (!init)
+	{
+		if (SUCCEEDED(pSwapChain->GetDevice(__uuidof(ID3D11Device), (void**)& pDevice)))
+		{
+			pDevice->GetImmediateContext(&pContext);
+			DXGI_SWAP_CHAIN_DESC sd;
+			pSwapChain->GetDesc(&sd);
+			window = sd.OutputWindow;
+			ID3D11Texture2D* pBackBuffer;
+			pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)& pBackBuffer);
+			pDevice->CreateRenderTargetView(pBackBuffer, NULL, &mainRenderTargetView);
+			pBackBuffer->Release();
+			oWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
+			InitImGui();
+			init = true;
+		}
 
-    ImGui_ImplDX11_NewFrame();
-    ImGui_ImplWin32_NewFrame();
-    ImGui::NewFrame();
+		else
+			return oPresent(pSwapChain, SyncInterval, Flags);
+	}
 
-    ImGui::Begin("Thuggy");
-    ImGui::End();
+	ImGui_ImplDX11_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
 
-    ImGui::Render();
+	ImGui::Begin("Thuggy");
+    // ImGui::ShowDemoWindow();
+	ImGui::End();
 
-    context_ptr->OMSetRenderTargets(1, &main_render_target_view_ptr, NULL);
-    ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-    return o_present(swap_chain_ptr, sync_interval, flags);
+	ImGui::Render();
+
+	pContext->OMSetRenderTargets(1, &mainRenderTargetView, NULL);
+	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+	return oPresent(pSwapChain, SyncInterval, Flags);
 }
 
-DWORD WINAPI main_thread(LPVOID lp_reserved){
-    bool init_hook = false;
-    do{
-        if(kiero::init(kiero::RenderType::D3D11) == kiero::Status::Success){
-            kiero::bind(8, (void**)& o_present, present_hook);
-            init_hook = true;
-        }
-    }while (!init_hook);
-    return TRUE;
+DWORD WINAPI MainThread(LPVOID lpReserved)
+{
+	bool init_hook = false;
+	do
+	{
+		if (kiero::init(kiero::RenderType::D3D11) == kiero::Status::Success)
+		{
+			kiero::bind(8, (void**)& oPresent, hkPresent);
+			init_hook = true;
+		}
+	} while (!init_hook);
+	return TRUE;
 }
 
-BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved) {
-    switch (dwReason)
-    {
-    case DLL_PROCESS_ATTACH:
-        DisableThreadLibraryCalls(hMod);
-        CreateThread(nullptr, 0, main_thread, hMod, 0, nullptr);
-        break;
-    
-    case DLL_PROCESS_DETACH:
-    kiero::shutdown();
-        break;
-    }
-    return TRUE;
+BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hMod);
+		CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
+		break;
+	case DLL_PROCESS_DETACH:
+		kiero::shutdown();
+		break;
+	}
+	return TRUE;
 }
